3c8cc1d0629242916ee3dd992d081b32f512969b
Romaisa alvi <romaisa.uchiha@gmail.com>
2023-10-25 01:15:26 +0500

    front-end update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import './App.css'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <>
-      <h1>React</h1>
-    </>
-  )
-}
+    <div className="App">
+      <BrowserRouter>
+        <Navbar/>
+        <div className="pages">
+          <Routes>
+            <Route 
+              path='/'
+              element={<Home/>}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
