92d910933bca66b3e5a0a3d50c8f7617a3b6277b
Romaisa alvi <romaisa.uchiha@gmail.com>
2023-10-25 04:27:57 +0500

    updating crud functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import WorkoutCard from '../components/WorkoutCard'
+import WorkoutForm from "../components/WorkoutForm";
+
 const Home = () => {
+    const [workouts,setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const getWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if(response.ok){
+                setWorkouts(json)
+            }
+        }
+        getWorkouts()
+    },[])
     return (
-        <div className="Home">
-            <h1>Home</h1>
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map(workout => <WorkoutCard key={workout._id} workout={workout} />)}
+            </div>    
+            <WorkoutForm/>
         </div>
     );
 };
